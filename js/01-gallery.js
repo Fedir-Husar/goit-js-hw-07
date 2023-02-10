2e8a5ae1d1bf3f84e20c779e30d5662974bc54f1
Fedir-Husar <105937067+Fedir-Husar@users.noreply.github.com>
2023-02-10 10:39:57 +0200

    Finish first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imageMarcup = createImageGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imageMarcup);
+
+function createImageGalleryMarkup(galleryItems){
+
+    return galleryItems.map(({preview, original, description}) =>{
+        return`
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+
+  </a>
+</div>`;
+    }).join('');
+
+}
+
+galleryContainer.addEventListener('click', openModal);
+
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', closeModal);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', closeModal);
+      },
+    }
+  );
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+  instance.show();
+}
+
+
+
